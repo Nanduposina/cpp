0f71860635466c6c11e889d97fff89b504110b50
Nandu posina <137518460+Nanduposina@users.noreply.github.com>
2023-10-14 10:46:55 +0530

    Add files via upload

---
+++ factors_of_num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,i;
+    cout<<"Enter the number to check factors of it:";
+    cin>>num;
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
